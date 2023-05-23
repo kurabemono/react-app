53910847c81b256b16c8398bf25730803bb76e94
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-05-23 14:19:26 +0900

    After Ch5-10

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import { useState } from "react";
-import Like from "./components/Like";
-//import "./App.css";
+import { produce } from "immer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <Like onClick={() => console.log("Clicked")} />;
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
+
+  const handleClick = () => {
+    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
+  };
+
+  return (
+    <div>
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
+      <button className="btn btn-primary" onClick={handleClick}>
+        Click me
+      </button>
+    </div>
+  );
 }
 
 export default App;
