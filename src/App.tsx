90ed2327ab4aa027a17633fb11ca0ef89bfb82bf
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-05-23 14:48:05 +0900

    After Ch5-12 Exer. 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import { produce } from "immer";
 //import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
+
+  const handleClick = () => {
+    // game.player.name = "Bob";
+
+    // using vanilla React
+    //setGame({ ...game, player: { ...game.player, name: "Bob" } });
+
+    // using immer
+    setGame(
+      produce((draft) => {
+        draft.player.name = "Bob";
+      })
+    );
+  };
 
   return (
-    <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
-    </div>
+    <>
+      <p key={game.player.name}>Player name: {game.player.name}</p>
+      <button onClick={handleClick}>Click me</button>
+    </>
   );
 }
 
