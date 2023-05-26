c358e4efc950909e08a940ac9cf1c87586fb0036
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-05-26 13:41:56 +0900

    After Ch6-4

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,16 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
 
 const Form = () => {
+  // useRef<class> to declare to TypeScript what element is being referenced. used to prevent compilation errors when accessing properties of the reference later in code (ex. nameRef.current.value)
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Submitted");
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    console.log(person);
   };
   // div.mb-3>label.form-label+input.form-control
   return (
@@ -12,13 +19,13 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={nameRef} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
