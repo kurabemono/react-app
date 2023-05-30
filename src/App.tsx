847689d2476990e5dcbced4e459eb5121ec6df92
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-05-30 11:34:13 +0900

    After Ch7-3

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,21 @@
 import { useState } from "react";
-import ExpenseFilter from "./components/ExpenseFilter";
-import ExpenseList from "./components/ExpenseList";
-import ExpenseForm from "./components/ExpenseForm";
-import { FieldValues } from "react-hook-form";
-//import "bootstrap/dist/css/bootstrap.min.css";
+import ProductList from "./components/ProductList";
 
 function App() {
-  // const handleClick = () => {};
-  const [filter, setFilter] = useState("");
-
-  const handleFilterChange = (filterKeyword: string) => {
-    setFilter(filterKeyword);
-  };
-
-  const handleDelete = (uuid: string) => {
-    setExpenses(expenses.filter((expense) => expense.id !== uuid));
-  };
-
-  const handleSubmit = (data: FieldValues) => {
-    setExpenses([
-      ...expenses,
-      {
-        id: crypto.randomUUID(),
-        description: data.description,
-        amount: data.amount,
-        category: data.category,
-      },
-    ]);
-  };
-
-  const [expenses, setExpenses] = useState([
-    {
-      id: crypto.randomUUID(),
-      description: "Milk",
-      amount: 5,
-      category: "Groceries",
-    },
-    {
-      id: crypto.randomUUID(),
-      description: "Eggs",
-      amount: 3,
-      category: "Groceries",
-    },
-    {
-      id: crypto.randomUUID(),
-      description: "Netflix",
-      amount: 12,
-      category: "Entertainment",
-    },
-  ]);
+  const [category, setCategory] = useState("");
 
   return (
-    <>
-      <div className="mb-3">
-        <ExpenseForm onSubmit={handleSubmit} />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter onChangeFilter={handleFilterChange} />
-      </div>
-      <div className="mb-3">
-        <ExpenseList
-          expenses={expenses}
-          filterKeyword={filter}
-          onDelete={handleDelete}
-        />
-      </div>
-    </>
+    <div>
+      <select
+        className="form-select"
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
+    </div>
   );
 }
 
